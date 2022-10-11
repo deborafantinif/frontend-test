08162d68085f80f9439922ac192375eef8426658
Debora Fantini <deboraferfan@gmail.com>
2022-10-11 14:01:10 -0300

    STYLE: css do componente HeaderDetails

--- ghibli-search/src/components/HeaderDetails.tsx
+++ ghibli-search/src/components/HeaderDetails.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Menu } from './Menu';
+import styles from '../styles/HeaderDetails.module.css'
 
 export function HeaderDetails() {
   return (
-    <div>
+    <header className={styles.header}>
       <Menu/>
       <h1>GHIBLI SEARCH</h1>
-    </div>
+    </header>
   )
 }

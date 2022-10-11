9b02364a2fd5e3f4e7f9023519121afebb0701f2
Debora Fantini <deboraferfan@gmail.com>
2022-10-11 17:18:03 -0300

    STYLE: css do componente Loading

--- ghibli-search/src/components/Loading.tsx
+++ ghibli-search/src/components/Loading.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from '../styles/Loading.module.css';
 
 export function Loading() {
   return (
-    <div>
-      <p>Loading...</p>
+    <div className={styles.loading}>
+      <span className={styles.loader}></span>
+      <h3>Loading...</h3>
     </div>
   )
 }

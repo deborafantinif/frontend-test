e2e3e520f6c7616e6c59eef89856d4b540419b99
Debora Fantini <deboraferfan@gmail.com>
2022-10-12 17:39:52 -0300

    FIX[images e loading]: - mudança no loading - remoção de favicons extras

--- ghibli-search/src/components/Loading.tsx
+++ ghibli-search/src/components/Loading.tsx
@@ -1,10 +1,11 @@
+import { CircleNotch } from 'phosphor-react';
 import React from 'react';
 import styles from '../styles/Loading.module.css';
 
 export function Loading() {
   return (
     <div className={styles.loading}>
-      <span className={styles.loader}></span>
+      <CircleNotch size={30} className={styles.loader}/>
       <h3>Loading...</h3>
     </div>
   )
